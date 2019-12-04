5c303f61ad63b53304074603de44cbb5e0035dc8
wangyongshuai <619926668@qq.com>
2019-12-04 19:19:05 +0800

    第一次提交

---
+++ src/main/java/com/wys/test/MainTest.java
@@ -0,0 +1,15 @@
+package com.wys.test;
+
+/**
+ *
+ * @作者: wys
+ * @时间: 2019年12月4日
+ *
+ */
+public class MainTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Word");
+	}
+}
